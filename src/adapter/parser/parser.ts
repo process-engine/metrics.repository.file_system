4ff47ff12bd40fac045759bd2bdb2641d6b7d710
ElRaptorus <christian.werner@5minds.de>
2019-07-10 09:22:10 +0200

    :sparkles: Implement dedicated parsers for diffent versions of logentries

---
+++ src/adapter/parser/parser.ts
@@ -0,0 +1,17 @@
+import {Metric} from '@atlas-engine/metrics_api.contracts';
+
+import {parseFlowNodeInstanceMetric} from './flow_node_metric_parser';
+import {parseProcessModelMetric} from './process_model_metric_parser';
+
+export function parseMetric(metricRaw: string): Metric {
+
+  const metricRawParts = metricRaw.split(';');
+
+  const isFlowNodeInstanceMetric = metricRawParts[0].startsWith('FlowNodeInstance');
+
+  const metric = isFlowNodeInstanceMetric
+    ? parseFlowNodeInstanceMetric(metricRawParts)
+    : parseProcessModelMetric(metricRawParts);
+
+  return metric;
+}
